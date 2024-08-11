0b642050eb0196e410dc86d3b04c48df93d2e36b
Elbadil <elbaliadil@gmail.com>
2024-08-11 17:59:39 +0100

    Created the add job Form to register new jobs in the json file

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import AddJobPage from "./pages/AddJobPage";
+import JobPage, { jobLoader } from "./components/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
+      <Route path="/add-job" element={<AddJobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
